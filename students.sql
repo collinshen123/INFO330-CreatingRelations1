6441c6e37a2f96f2a65dc6422a2765bd63536990
collinshen123 <collinshen123@gmail.com>
2023-04-12 11:12:30 -0700

    edits

---
+++ students.sql
@@ -0,0 +1,9 @@
+Create Table If Not Exists students (ID Integer Primary Key, first_name String(40), last_name String(80), age Numeric)
+Insert Into students (ID, first_name, last_name, age) Values (1, 'Fred', 'Flintstone', 35);
+Insert Into students (ID, first_name, last_name, age) Values (2, 'Wilma', 'Flintstone', 29);
+Insert Into students (ID, first_name, last_name, age) Values (3, 'Barney', 'Rubble', 33);
+Insert Into students (ID, first_name, last_name, age) Values (4, 'Betty', 'Rubble', 29);
+Insert Into students (ID, first_name, last_name, age) Values (5, 'Pebbles', 'Flintstone', 1);
+Insert Into students (ID, first_name, last_name, age) Values (6, 'Bam-Bam', 'Rubble', 1);
+Insert Into students (ID, first_name, last_name, age) Values (7, 'Collin', 'Shen', 19);
+Insert Into students (ID, first_name, last_name, age) Values (8, 'Jackie', 'Shen', 1);
